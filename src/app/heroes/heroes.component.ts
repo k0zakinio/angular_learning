7947fb699978c2d12d338485da85fde5af8d0045
k0zakinio <adowellphoto@gmail.com>
2018-05-30 00:48:26 +0100

    Chapter 7 completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -9,8 +8,6 @@ import {Observable} from 'rxjs';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) {
@@ -24,9 +21,4 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-
 }
